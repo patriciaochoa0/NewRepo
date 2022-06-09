bad90e5636388506513ff59261bf0ee3b670c8ad
Patricia Ochoa <patriciaochoa@Patricias-MacBook-Pro.local>
2022-06-09 17:19:00 -0400

    will this work?

--- .github/workflows/bash.sh
+++ .github/workflows/bash.sh
@@ -3,12 +3,17 @@ set -ex
 
 regionFlag=$1
 
+
 if [[ $regionFlag == "US" ]]; then 
     echo $regionFlag THIS IS THE regionFlag of US
+fi
 if [[ $regionFlag == "EU" ]]; then 
     echo $regionFlag THIS IS THE regionFlag of EU
+fi
 
-
+if [[ $2 == "heads/ref/somebranchname"]]; then
+    echo "I'm on the somebrnachname branh"
+fi
 
 
 # #!/bin/sh
