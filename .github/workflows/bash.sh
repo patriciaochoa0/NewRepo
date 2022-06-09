535b0105f216176018137ea1d2d5ec0af398a2b8
Patricia Ochoa <patriciaochoa@Patricias-MacBook-Pro.local>
2022-06-09 14:39:40 -0400

    testing bash

---
+++ .github/workflows/bash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+regionFlag=$1
+
+if [[ $regionFlag == "US" ]]; then 
+    echo $regionFlag THIS IS THE regionFlag of US
+if [[ $regionFlag == "EU" ]]; then 
+    echo $regionFlag THIS IS THE regionFlag of EU
+
+
+
+
+# #!/bin/sh
+# msg1=$
+# ms="$msg1 msg1"
+# msg2="$ms two"
+# msg3="$msg2 three"
+# echo $msg3
